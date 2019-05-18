d3b108edb1141a58c263ec60899fe134a1d6c9bc
Diego Mazzone <diego@houlak.com>
2019-05-18 20:05:58 -0300

    Refactor

--- lib/templates/default/swift/Controllers/OtherLogin/OtherRegisterVC.swift
+++ lib/templates/default/swift/Controllers/OtherLogin/OtherRegisterVC.swift
@@ -151,7 +151,7 @@ class OtherRegisterVC: UITableViewController, UITextFieldDelegate {
     
     @IBAction func termsAndConditionsTapped(_ sender: UIButton) {
         // TODO: View url
-        if let url = URL(string: "http://notatourapp.com/terms-and-conditions") {
+        if let url = URL(string: "http://myproject.com/terms-and-conditions") {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
